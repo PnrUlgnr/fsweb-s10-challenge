ec2300b33a04196d1517fcf4f92a6042e175fe1b
Pinar Ülgener <pnrulgnr@gmail.com>
2024-02-05 16:20:54 +0300

    redux setup logger thunk

--- src/index.js
+++ src/index.js
@@ -1,17 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import { BrowserRouter } from 'react-router-dom';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { BrowserRouter } from "react-router-dom";
 
+import { legacy_createStore as createStore, applyMiddleware } from "redux";
+import { Provider } from "react-redux";
+import { thunk } from "redux-thunk";
+import rootReducer from "./index.js";
+import minnetReducer from "./reducers.js";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-
+const store = createStore(minnetReducer, applyMiddleware(thunk));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <BrowserRouter>
-    <>
+    <Provider store={store}>
       <App />
-    </>
+    </Provider>
   </BrowserRouter>
 );
